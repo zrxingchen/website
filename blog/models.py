adc3fe931b6358093d618b74ed4cfaded0d6f58e
Dom <2425277916@qq.com>
2020-09-17 19:12:03 +0800

    '第一步测试'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,64 @@
+import uuid
+import os
+# 导入django 模块
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+#引用第三方模块
 
 # Create your models here.
+# 文章分类 表
+class Category(models.Model):
+    name = models.CharField(max_length=255,verbose_name='类别名称')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+
+#最大限度的避免图片被覆盖
+def article_img_path(instance,filename):
+    # 截取后缀名
+    ext = filename.split('.')[-1]
+    # 计算uuid 截取 32位
+    filename = '{}.{}'.format(uuid.uuid4(),ext)
+    # return '{0}/{1}/{2}'.format(instance.user.id,"avatar",filename)
+    return os.path.join('avatar',filename)
+    # 返回前置为用户名 后置为文件名
+
+#  创建文章表
+class Article(models.Model):
+    title = models.CharField(verbose_name='文章标题',max_length=50)
+    author = models.ForeignKey(User,verbose_name='作者',on_delete=models.CASCADE)
+    img = models.ImageField(upload_to=article_img_path,null=True,blank=True,verbose_name="文章配图")
+    content = models.TextField(verbose_name='文章内容')
+    abstract = models.TextField(verbose_name='文章摘要',null=True,blank=True,max_length=255)
+    visited = models.PositiveIntegerField(verbose_name='访问量',default=0)
+    category = models.ManyToManyField('Category',verbose_name='文章分类')
+    created_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
+    updated_time = models.DateTimeField(auto_now=True,verbose_name='更新时间')
+
+    class Meta:
+        verbose_name = '文章内容'
+        verbose_name_plural = verbose_name
+        ordering = ['-created_time']
+
+
+    def __str__(self):
+        return self.title
+
+    # 可以通过调用这个函数 ，直接返回详情页的url地址
+    def get_absolute_url(self):
+        return reverse("blog:blog_detail",kwargs={'a_id':self.id})
+
+    # 访问量+1
+    def increase_visited(self):
+        self.visited += 1
+        self.save(update_fields=['visited'])
+
+
+
+
--- myblog/settings.py
+++ myblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'g_@hrft+(vbm-(1(txacqvszo+964-h3+id($4%yvgas7^zh_2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +78,15 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog',
+        'USER': 'root',
+        'PASSWORD':'2020312',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
+
 
 
 # Password validation
@@ -103,18 +111,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'zh-Hans'
+
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
